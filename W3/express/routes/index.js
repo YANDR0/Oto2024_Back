e0fac88fc4061ce66a41aab7f51dd463a81ac2f0
RODRIGUEZ BARRETO <yael.rodriguez@iteso.mx>
2024-08-28 10:48:17 -0600

    pedon

--- W3/express/routes/index.js
+++ W3/express/routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const users = require('./users');
 const projects = require('./projects');
 
+router.use(express.json())
+
 router.get('/', (req, res) => {
     res.send('Pagina de inicio')
 })
--- W3/express/routes/users.js
+++ W3/express/routes/users.js
@@ -1,7 +1,13 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Lista de usuarios')
+})
+
+router.get('/:id', (req, res) => {
+    const userID = req.params.id
+    res.send(`Datos del ususario ${userID}`)
 })
 
 router.post('/', (req, res) => {
